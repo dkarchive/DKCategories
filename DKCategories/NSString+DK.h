bd3c058f0712ab8395eed32ce472823d6c35bc2e
dkhamsing <danielkhamsing@gmail.com>
2014-01-30 06:48:17 -0800

    Add method to truncate to width with font

--- DKCategories/NSString+DK.h
+++ DKCategories/NSString+DK.h
@@ -25,12 +25,22 @@
  */
 - (NSString *)dk_pluralize:(NSInteger)number;
 
+
 /**
  Returns the person pluralization for the given `numberOfPeople`.
  @param numberOfPeople Number of people to pluralize
  @return A string with the correct person pluralization
  */
 + (NSString *)dk_pluralizePerson:(NSInteger)numberOfPeople;
+
+
+/**
+ Truncates receiver to given width.
+ Credit: http://stackoverflow.com/questions/10693383/truncate-an-nsstring-to-width
+ @param width Width to truncate string to
+ @param font Font to use in computing width
+ */
+- (NSString*)dk_truncateToWidth:(CGFloat)width withFont:(UIFont*)font;
 
 
 @end
