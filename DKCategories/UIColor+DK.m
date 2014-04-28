b577c6e7c8715db5258580bd6bf6f615fd0bcfef
dkhamsing <danielkhamsing@gmail.com>
2014-04-28 12:44:22 -0700

    Handle missing #

--- DKCategories/UIColor+DK.m
+++ DKCategories/UIColor+DK.m
@@ -22,11 +22,17 @@
 
 
 + (UIColor *)dk_colorWithHexString:(NSString *)hexString {
+    NSString *firstCharacter = [hexString substringToIndex:1];
+    if (![firstCharacter isEqualToString:@"#"]) {
+        hexString = [hexString stringByAppendingString:@"#"];
+    }
+    
     unsigned rgbValue = 0;
     NSScanner *scanner = [NSScanner scannerWithString:hexString];
     [scanner setScanLocation:1]; // bypass '#' character
     [scanner scanHexInt:&rgbValue];
     return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 }
+
 
 @end
